900f4870ca4402269ac95787dc29ff2ecc362647
Priyanka Asthana <166942678+PriyankaAsthana@users.noreply.github.com>
2025-05-01 21:16:05 +0530

    Update Print_primes.java

--- Basic_Questions/Print_primes.java
+++ Basic_Questions/Print_primes.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+
+public class javab {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter start of range: ");
+        int start = sc.nextInt();
+        System.out.print("Enter end of range: ");
+        int end = sc.nextInt();
+
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
 
