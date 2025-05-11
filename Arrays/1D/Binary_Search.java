a22a4ad38769de02ac73cd16740ea6228ab667cb
Priyanka Asthana <166942678+PriyankaAsthana@users.noreply.github.com>
2025-05-11 20:14:57 +0530

    Create Binary_Search.java

---
+++ Arrays/1D/Binary_Search.java
@@ -0,0 +1,19 @@
+public class javab{
+public static int binarySearch(int[] arr, int target) {
+    int low = 0, high = arr.length - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            return mid; // Found at index mid
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1; // Not found
+  }
+}
